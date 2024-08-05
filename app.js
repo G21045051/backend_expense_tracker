613ba22e56bd2455af075b502f18e7da7adf6ac5
gk <gyanendra.kumar.che21@itbhu.ac.in>
2024-08-05 12:16:09 +0530

    second-commit

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ app.use(cors());
 
 //routes
 readdirSync('./routes').map(route => app.use('/api/v1', require('./routes/' + route)));
-app.get("/",(req,res)=>
-    {
-    res.send("hello world");
-}
-)
+ 
 
 const server = () => {
     db();

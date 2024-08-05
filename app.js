60e6d4f9cc9e9f1faa29463bf8c69f016d269276
gk <gyanendra.kumar.che21@itbhu.ac.in>
2024-08-05 12:04:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+const { readdirSync } = require('fs');
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//routes
+readdirSync('./routes').map(route => app.use('/api/v1', require('./routes/' + route)));
+app.get("/",(req,res)=>
+    {
+    res.send("hello world");
+}
+)
+
+const server = () => {
+    db();
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT);
+    });
+};
+
+server();
